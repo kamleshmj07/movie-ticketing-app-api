7ecb344718e12f074060b94989c2229ba2f400af
Kamlesh Mj <kamleshmj07@gmail.com>
2021-11-25 10:23:43 +0530

    Before running tests for user

---
+++ app/bookingapp/tests/test_admin.py
@@ -0,0 +1,28 @@
+from django.forms.fields import EmailField
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@gmail.com',
+            password='admin123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@gmail.com',
+            password='test123',
+            name='testuser'
+        )
+
+    def test_users_liste(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
